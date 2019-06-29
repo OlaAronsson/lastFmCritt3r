68616ca609670a1651008f767cb50a1fdd3e5e83
ola <ola@nollettnoll.net>
2019-06-29 10:01:25 +0200

    adding the thing

---
+++ src/tweet_last_FM.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER=MYLASTFMUSER
+KEY=MYLASTFMAPIKEY
+
+# Main
+/usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=♬ This week's top spins on $USER's Last FM
+`curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | head -5`" | jq
+exit 0
+
