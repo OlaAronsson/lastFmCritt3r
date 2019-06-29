3c835bf44551f01ed23d3d96bc382e54ce247a3b
ola <ola@nollettnoll.net>
2019-06-29 12:04:44 +0200

    adding s simple compact mode

--- README.md
+++ README.md
@@ -25,5 +25,11 @@ That's it. It should work fine, posting stuff like
 4. Cocteau Twins : Cherry-Coloured Funk  
 5. Cocteau Twins : The Itchy Glowbo Blow  
 
-to your twitter account. Now just crontab -e and set it up for a weekly run.
+or
+
+♬ olamattias's Last FM says (1) Damon Albarn:The Selfish Giant (2) Jai Paul:BTSTU - Demo (3) Jai Paul:Zion Wolf Theme - Unfinished (4) Cocteau Twins:Cherry-Coloured Funk (5) Cocteau Twins:The Itchy Glowbo Blow
+
+to your twitter account having COMPACT toggled to 0.
+
+Now just crontab -e and set it up for a weekly run.
 
--- src/tweet_last_FM.sh
+++ src/tweet_last_FM.sh
@@ -2,9 +2,16 @@
 
 USER=MYLASTFMUSER
 KEY=MYLASTFMAPIKEY
+COMPACT=1
 
 # Main
-/usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=♬ This week's top spins on $USER's Last FM
+if [ $COMPACT -eq 0 ]; then
+  /usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=�~Y� $USER's Last FM says `curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed 's/^/(/' | sed '/_/{s/_/) /1}' | sed s#_#':'#g | head -5 | tr '\n' ' '`" | jq
+else
+  /usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=�~Y� This week's top spins on $USER's Last FM
 `curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed '/_/{s/_/. /1}' | sed s#_#' : '#g | head -5`" | jq
+fi
+
 exit 0
+
 
