9c5bc75b7636747e66654be5b2eda75fe84a268b
ola <ola@nollettnoll.net>
2019-06-29 17:45:52 +0200

    added no of lines option

--- src/tweet_last_FM.sh
+++ src/tweet_last_FM.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# Required
 USER=MYLASTFMUSER
 KEY=MYLASTFMAPIKEY
-COMPACT=1
+
+# Options
+COMPACT=0
+NOOFLINES=5
 
 # Main
 if [ $COMPACT -eq 0 ]; then
-  /usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=�~Y� $USER's Last FM says `curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed 's/^/(/' | sed '/_/{s/_/) /1}' | sed s#_#':'#g | head -5 | tr '\n' ' '`#lastFmCritt3r" | jq
+  /usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=�~Y� $USER's Last FM says `curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | head -$NOOFLINES | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed 's/^/(/' | sed '/_/{s/_/) /1}' | sed s#_#':'#g | tr '\n' ' '`#lastFmCritt3r" | jq
 else
   /usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=�~Y� This week's top spins on $USER's Last FM
-`curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed '/_/{s/_/. /1}' | sed s#_#' : '#g | head -5`#lastFmCritt3r" | jq
+`curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | head -$NOOFLINES | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed '/_/{s/_/. /1}' | sed s#_#' : '#g`#lastFmCritt3r" | jq
 fi
+
 
 exit 0
 
