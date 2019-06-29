09e9bba0386a6f17bb59753fe2d586e77fb63ee9
ola <ola@nollettnoll.net>
2019-06-29 21:13:22 +0200

    twurl exec chk

--- src/tweet_last_FM.sh
+++ src/tweet_last_FM.sh
@@ -12,11 +12,15 @@ NOOFTRACKS=5
 echo $USER | grep "MYLASTFM" > /dev/null 2>&1 && echo "You need to provide your Last FM User" && exit 1
 echo $KEY | grep "MYLASTFM" > /dev/null 2>&1 && echo "You need to provide your Last FM API key" && exit 1
 
+# Path to twurl
+TWURL=/usr/local/bin/twurl
+[ ! -x $TWURL ] && echo "configured twurl path $TWURL is not executable" && exit 1
+
 # Main
 if [ $COMPACT -eq 0 ]; then
-  /usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=♬ $USER's Last FM says `curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | head -$NOOFTRACKS | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed 's/^/(/' | sed '/_/{s/_/) /1}' | sed s#_#':'#g | tr '\n' ' '`#lastFmCritt3r" | jq
+  $TWURL -X POST -H api.twitter.com "/1.1/statuses/update.json?status=♬ $USER's Last FM says `curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | head -$NOOFTRACKS | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed 's/^/(/' | sed '/_/{s/_/) /1}' | sed s#_#':'#g | tr '\n' ' '`#lastFmCritt3r" | jq
 else
-  /usr/local/bin/twurl -X POST -H api.twitter.com "/1.1/statuses/update.json?status=♬ This week's top spins on $USER's Last FM
+  $TWURL -X POST -H api.twitter.com "/1.1/statuses/update.json?status=♬ This week's top spins on $USER's Last FM
 `curl -s  "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&user=$USER&api_key=$KEY&format=json" | sed s='#'==g | sed s='@'==g | jq -r '[.weeklytrackchart.track[] | .attr.rank, .artist.text, .name]' | grep -v "\]" | grep -v "\[" | paste -d" " - - - | head -$NOOFTRACKS | cut -b 3- | sed s#'",'#_#g | sed s#'"'##g | sed s#'  '##g | sed s#'_ '#'_'#g | sed 's/.$//' | sed '/_/{s/_/. /1}' | sed s#_#' : '#g`#lastFmCritt3r" | jq
 fi
 
