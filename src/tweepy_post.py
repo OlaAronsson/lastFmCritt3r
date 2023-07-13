3bd69acbd3d31c38908162a0e411d42f5cf8a9e9
Ola Aronsson <ola@nollettnoll.net>
2023-07-14 01:46:39 +0200

    removed duplicate code, clearer description on the parasms

--- src/tweepy_post.py
+++ src/tweepy_post.py
@@ -1,25 +1,34 @@
 import tweepy
 
+def __do_post(tweet: str, client: tweepy.Client):
+    response = client.create_tweet(
+        text=tweet
+    )
+    resp_id = str({response.data['id']})    
+    if bool (resp_id and resp_id.strip()):
+        print("0")
+    else:
+        print("1")      
+
 def __post_inner(tweet: str, silent: bool):
     
     if bool (tweet and tweet.strip()):
 
         ########################################################
         #
-        # Below details should be, while
+        # Below variables can be found at
         #
-        # a) being logged in
-        # b) having setup your access tokens for read AND write
-        # at your project page reached from
+        #  https://developer.twitter.com/en/portal/dashboard
         #
-        # https://developer.twitter.com/en/portal/dashboard
+        # Make sure having setup your access tokens for read
+        # _and_ write : default they are read only.
         #
         ########################################################
         
-        consumer_key = "TWITTER_API_KEY"
-        consumer_secret = "TWITTER_API_SECRET"
-        access_token = "TWITTER_ACCESS_TOKEN"
-        access_token_secret = "TWITTER_ACCESS_TOKEN_SECRET"
+        consumer_key = "1Y9oW2oaWK8SbhGSAEJm9iyQl"
+        consumer_secret = "6ay6AJBfZUty02NRNxfdq1JS6inVKi5OTTonhA8mP5cYZrxD4C"
+        access_token = "4193726788-TpWIXzYB68QwxKvlxl3lkLkuxhr6nnnG5Ak3nWn"
+        access_token_secret = "lMDvZfsAMFGjKZIgAujcyoW6B5c7mpCuWUEhdgcx5LO9U"
 
         client = tweepy.Client(
             consumer_key=consumer_key, consumer_secret=consumer_secret,
@@ -27,25 +36,11 @@ def __post_inner(tweet: str, silent: bool):
 
         if silent:
             try:
-                response = client.create_tweet(
-                    text=tweet
-                )
-                resp_id = str({response.data['id']})    
-                if bool (resp_id and resp_id.strip()):
-                    print("0")
-                else:
-                    print("1")
+                __do_post(tweet, client)                
             except:
                 print("1")
         else:
-            response = client.create_tweet(
-                text=tweet
-            )
-            resp_id = str({response.data['id']})    
-            if bool (resp_id and resp_id.strip()):
-                print("0")
-            else:
-                print("1")            
+            __do_post(tweet, client)
     else:        
         print("1")
 
